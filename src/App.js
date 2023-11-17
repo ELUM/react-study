4e59e6284690367ee082d1f32851005b4fe0b8ad
Mule <1533238648@qq.com>
2023-11-17 17:18:57 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>{bornYear()}</p>
+      <p>Hello{name}{age}</p>
+    </div>
+  )
+}
 
 function App() {
+  const now = new Date()
+  const [age, setAge] = useState(0)
+
+  // setTimeout(() => {
+  //   console.log(age);
+  //   setAge(age + 1)
+  // }, 1000)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {now.getDate()}
+      <p>This is React</p>
+      <Hello name="你好" age={age + 10} />
+      <Hello name="你好" age={age + 2} />
+      <Hello name="你好" age={age + 5} />
+      <Hello name="你好" age={age + 8} />
     </div>
   );
 }
